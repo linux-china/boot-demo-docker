027acbc633aaadff614ed637ce717db6c5bfe429
linux_china <linux_china@hotmail.com>
2016-06-19 16:57:27 -0700

    application testcase

---
+++ src/test/java/org/mvnsearch/DockerDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package org.mvnsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DockerDemoApplication.class)
+@WebAppConfiguration
+public class DockerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
